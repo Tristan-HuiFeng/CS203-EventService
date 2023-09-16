28f5a5ae478b8af68bf11087ae5ef03d8187fe36
Wesly_Chau_Li_Zhan <118585815+weslychau-01@users.noreply.github.com>
2023-09-16 14:51:15 +0800

    Ticket Type Service
    - TicketType CRU
    - Integration Test
    - Unit Test
    
    ---------
    
    Co-authored-by: Chew Hui Feng <tristan.chf@gmail.com>

---
+++ src/main/java/com/eztix/eventservice/model/TicketType.java
@@ -0,0 +1,52 @@
+    package com.eztix.eventservice.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="TicketType")
+@Table(name= "TICKETTYPE")
+public class TicketType {
+
+    @Id
+    @SequenceGenerator(name = "ticketType_sequence", sequenceName = "ticketType_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ticketType_sequence")
+    @Schema(hidden = true)
+    private Long id;
+
+    @NotNull
+    @Column(name = "activity_id")
+    private Long activityId;
+
+    @NotNull
+    @Column(name = "type")
+    private String type;
+
+    @NotNull
+    @Column(name = "price")
+    private double price;
+
+    @NotNull
+    @Column(name = "total_vacancy")
+    private int total_vacancy;
+
+    @NotNull
+    @Column(name = "occupied_count")
+    private int occupied_count;
+
+    @NotNull
+    @Column(name = "reserved_count")
+    private int reserved_count;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+    
+}
