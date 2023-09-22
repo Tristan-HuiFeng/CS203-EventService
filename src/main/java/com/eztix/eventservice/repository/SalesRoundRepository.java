2d6bb34ffb96ec06813ff328b74d89f82b45e979
Wesly Chau Li Zhan <wesly.chau.li.zhan.01@gmail.com>
2023-09-22 11:34:30 +0800

    sales round controller repo service and mode

---
+++ src/main/java/com/eztix/eventservice/repository/SalesRoundRepository.java
@@ -0,0 +1,9 @@
+package com.eztix.eventservice.repository;
+
+import com.eztix.eventservice.model.SalesRound;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SalesRoundRepository extends CrudRepository<SalesRound, Long> {
+
+}
