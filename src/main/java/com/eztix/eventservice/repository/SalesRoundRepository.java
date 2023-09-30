d4b360ef5a616c2743630875f4a71a60e2f65514
Wesly Chau Li Zhan <wesly.chau.li.zhan.01@gmail.com>
2023-09-30 17:32:07 +0800

    attribute naming correction activityId -> eventId

--- src/main/java/com/eztix/eventservice/repository/SalesRoundRepository.java
+++ src/main/java/com/eztix/eventservice/repository/SalesRoundRepository.java
@@ -4,5 +4,9 @@ import com.eztix.eventservice.model.SalesRound;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SalesRoundRepository extends CrudRepository<SalesRound, Long> {
+
+    Optional<Iterable<SalesRound>> findByEventId(Long eventId);
 }
