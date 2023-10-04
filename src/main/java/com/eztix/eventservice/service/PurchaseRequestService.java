faa815ef3938aa8f9e2446b35b9178a0684dfe5e
Wesly Chau Li Zhan <wesly.chau.li.zhan.01@gmail.com>
2023-10-05 01:57:44 +0800

    updated error handling for rollback guarantee

--- src/main/java/com/eztix/eventservice/service/PurchaseRequestService.java
+++ src/main/java/com/eztix/eventservice/service/PurchaseRequestService.java
@@ -16,6 +16,7 @@ import com.eztix.eventservice.repository.TicketTypeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.SecureRandom;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.*;
@@ -159,15 +160,23 @@ public class PurchaseRequestService {
     }
 
     // PR algorithm to process purchase requests by assign queue number
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public void processPurchaseRequest(Long salesRoundId) throws ResourceNotFoundException{
+
+        // Retrieve purchase requests
         Optional<java.util.stream.Stream<PurchaseRequest>> allPurchaseRequests = purchaseRequestRepository.findBySalesRoundId(salesRoundId);
+
+        // Check if any purchase requests are retrieved
         if (!allPurchaseRequests.isPresent()) {
+
+            // If no purchase requests retrieved, check if sales round exists, throw relevant exception
             if (!salesRoundRepository.findById(salesRoundId).isPresent()) {
                 throw new ResourceNotFoundException(String.format("sales round with id %d does not exist.", salesRoundId));
             }
             throw new ResourceNotFoundException(String.format("purchase requests with sales round id %d do not exist.", salesRoundId));
         }
+
+        // Algorithm
         Stream<PurchaseRequest> streamOfPR = allPurchaseRequests.get();
         long size = streamOfPR.count();
         List<Long> rng = new ArrayList<>();
@@ -175,15 +184,22 @@ public class PurchaseRequestService {
         while (i <= size) {
             rng.add(i++);
         }
-        Collections.shuffle(rng, new Random(System.currentTimeMillis()));
+        Collections.shuffle(rng, new SecureRandom()));
         Iterator<Long> rngIterator = rng.iterator();
         List<PurchaseRequest> prToSave = new ArrayList<>();
         streamOfPR.forEach(pr -> {
             pr.setQueueNumber(rngIterator.next());
             prToSave.add(pr);
+
+        // included a try catch block in case any logic in error handling is missed out
+            // try {
+        // save purchase requests in batches of 50 to reduce database operations
             if (prToSave.size() % 50 == 0) {
                 purchaseRequestRepository.saveAll(prToSave);
             }
+            // } catch (Exception e) {
+            //     throw e;
+            // }
         });
 
         if (!prToSave.isEmpty()) {
