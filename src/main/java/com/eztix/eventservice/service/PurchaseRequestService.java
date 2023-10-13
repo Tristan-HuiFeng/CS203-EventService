5c896a8f76d2631585697c94eb17bccb2981871e
Bk49 <48423943+Bk49@users.noreply.github.com>
2023-10-13 21:20:45 +0800

    Resolve all conflict issues... (#24)

--- src/main/java/com/eztix/eventservice/service/PurchaseRequestService.java
+++ src/main/java/com/eztix/eventservice/service/PurchaseRequestService.java
@@ -1,7 +1,6 @@
 package com.eztix.eventservice.service;
 
 import com.eztix.eventservice.dto.PurchaseRequestDTO;
-import com.eztix.eventservice.dto.PurchaseRequestItemDTO;
 import com.eztix.eventservice.exception.RequestValidationException;
 import com.eztix.eventservice.exception.ResourceNotFoundException;
 import com.eztix.eventservice.model.PurchaseRequest;
@@ -11,11 +10,15 @@ import com.eztix.eventservice.model.TicketType;
 import com.eztix.eventservice.repository.PurchaseRequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.SecureRandom;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -113,6 +116,29 @@ public class PurchaseRequestService {
         return purchaseRequestRepository.save(currentPurchaseRequest);
     }
 
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
+    public void processPurchaseRequests(Long salesRoundId) {
+        // Get the total count of items for the given sales round
+        long totalItemCount = purchaseRequestRepository.countBySalesRoundId(salesRoundId);
+
+        // Algorithm
+        List<Long> rng = new ArrayList<>();
+        for (long i = 1; i <= totalItemCount; i++) {
+            rng.add(i);
+        }
+        Collections.shuffle(rng, new SecureRandom());
+
+        // Create an iterator for shuffled indices
+        Iterator<Long> rngIterator = rng.iterator();
+
+        // Stream through all the PRs under a sales round to assign queue numbers
+//        Stream<PurchaseRequest> prStream =
+                purchaseRequestRepository.findBySalesRoundId(salesRoundId)
+                        .peek(pr -> pr.setQueueNumber(rngIterator.next())) // Set Queue Number for each
+                        .sorted((a, b) -> Math.toIntExact(a.getQueueNumber() - b.getQueueNumber()))
+                        .forEach(purchaseRequestRepository::save); // Sort them by
+
+    }
     // Delete all PurchaseRequest
     public void deleteAllPurchaseRequests() {
         purchaseRequestRepository.deleteAll();
