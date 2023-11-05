00d0dc363e01a57f224d4122e355b797da59059e
Wesly Chau Li Zhan <wesly.chau.li.zhan.01@gmail.com>
2023-11-05 11:47:41 +0800

    activity service code comment

--- src/main/java/com/eztix/eventservice/controller/ActivityController.java
+++ src/main/java/com/eztix/eventservice/controller/ActivityController.java
@@ -14,9 +14,10 @@ public class ActivityController {
     private final ActivityService activityService;
 
     /**
+     * Retrieve an activity.
      * 
-     * @param activityId
-     * @return
+     * @param a long value representing the unique identifier of the activity to retrieve
+     * @return a ResponseEntity containing the retrieved Activity and an OK status
      */
     @GetMapping("/api/v1/activity/{activityId}")
     public ResponseEntity<Activity> getActivityById(@PathVariable Long activityId){
@@ -30,10 +31,11 @@ public class ActivityController {
 //    }
 
     /**
+     * Update the info of an activity.
      * 
-     * @param activityId
-     * @param activity
-     * @return
+     * @param activityId a long value representing the unique identifier of the activity to retrieve
+     * @param activity an Activity object containing the new Activity info to be updated
+     * @return a ResponseEntity containing the updated Activity and an OK status
      */
     @PutMapping("/api/v1/activity/{activityId}")
     public ResponseEntity<Activity> updateActivity(@PathVariable Long activityId, @RequestBody Activity activity ){
@@ -42,9 +44,10 @@ public class ActivityController {
     }
 
     /**
+     * Delete an activity.
      * 
-     * @param activityId
-     * @return
+     * @param activityId a long value representing the unique identifier of the activity to retrieve
+     * @return a ResponseEntity containing a string that states the activity deleted and its unique identifier and an OK status
      */
     @DeleteMapping("/api/v1/activity/{activityId}")
     public ResponseEntity<String> deleteActivity(@PathVariable Long activityId) {
